ec023457b676e656aee34aea5cb5367f65d02d7c
zachgobah <chiefgobah@gmail.com>
2026-05-22 17:35:11 -0400

    initial push

---
+++ upcCheckDigit/main.c
@@ -0,0 +1,20 @@
+/* Computes a Universal Product Code check digit. */
+
+#include <stdio.h>
+
+int main () 
+{
+	int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, first_sum, second_sum, total;
+
+	printf("Enter the first (single) digit: ");
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+
+	first_sum = ( d + i2 + i4 + j1 + j3 + j5);
+	second_sum = ( i1 + i3 + i5 + j2 + j4 );
+	total = (first_sum * 3.0 ) + second_sum - 1;
+	total = total % 10;
+	printf("UPC check digit: %d\n", (9 - total));
+
+	return 0;
+
+}
