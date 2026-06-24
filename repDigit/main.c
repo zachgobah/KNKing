9e27fbd9afe44c370c11a18b8b78bb30306193b7
zachgobah <chiefgobah@gmail.com>
2026-06-24 17:14:14 -0400

    8.3 complete though overflow still causes segfault

--- repDigit/main.c
+++ repDigit/main.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
-#include <stdbool.h>
 #define NUM_DIGITS 10
+#define MAX_SIZE 256
 
 int main(void) {
-	bool digit_seen[NUM_DIGITS] = {false};
-	int digit;
-	long n;
-	int i = 0;
-	int which_digits[NUM_DIGITS] = {0};
+	int n[MAX_SIZE];
+	int ch;
+	int i;
+	int count_digits[NUM_DIGITS] = {0};
 
-	printf("Enter a number: ");
-	scanf("%ld", &n);
+	while (1) {
+	
+		for (i = 0; i < MAX_SIZE; i++){
+			n[i] = -1;
+			if (i < NUM_DIGITS){
+				count_digits[i] = 0;
+			}
+		} i = 0;
 
-	while (n > 0) {
-		digit = n % 10;
-		if (digit_seen[digit]){
-			which_digits[i] = digit;
-			i++;
-			n /= 10;
-			continue;
-		}
-		digit_seen[digit] = true;
-		n /= 10;
-	}
+		printf("\nEnter a number (q to quit): ");
 
-	if (which_digits[0] == 0){
-		printf("No repeated digits.\n");
-	} else {
-		printf("Repeated digit(s): ");
-		for (i = 0; i < NUM_DIGITS; i++){
-			if (which_digits[i] == 0) {
-				printf("\n");
-				break;
-			} else {
-				printf("%d ", which_digits[i]);
+		while (i < MAX_SIZE && (ch = getchar()) != '\n' && ch != EOF){
+			if (ch == 'q' || ch == 'Q'){
+				return 0;
+			} else if (ch >= '0' && ch <= '9'){
+				n[i] = ch - '0';	// converts ASCII to int
+				i++;
 			}
 		}
-	}
+ 
+		for (i = 0; n[i] != -1 && i < MAX_SIZE; i++){
+			count_digits[n[i]]++;
+		}
+		
+		printf("Digit:\t\t");
+		for (i=0; i < NUM_DIGITS; i++){
+			printf("%4d", i);
+		}
+		printf("\nOccurences:\t");
 
+		for (i=0; i < NUM_DIGITS; i++){
+			printf("%4d", count_digits[i]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
 
